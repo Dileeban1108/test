89880923713b2642a95aaf595db00ff7aa492092
Dileeban s <dileebandileeban1108@gmail.com>
2023-07-29 22:05:31 +0530

    added php data types file successfully

---
+++ data types.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // String,integer,float,array
+    $myString="dileeban";
+    $myInt=34;
+    $myFloat=67.67;
+    echo $myString."<br>".$myInt."<br>".$myFloat."<br>";
+    $myArray=array('2','dileeban','cool','6.78','/','dilukshan');
+    echo $myArray['2'];
+    
+    
+    ?>
+</body>
+</html>

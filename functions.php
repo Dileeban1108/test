30e6232d23315032f14712a4231b9676931d4ecf
Dileeban S <dileebandileeban1108@gmail.com>
2023-08-06 15:18:47 +0530

    Added functions.php

---
+++ functions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+      echo strlen("dileeban")."<br>";
+      echo str_word_count("dileeban dilu kawsh")."<br>";
+      echo strrev("dileeban")."<br>";
+      echo strpos("my name is dileeban","dilu")."<br>";
+      echo sin(90)."<br>";
+      echo cos(90)."<br>";
+      echo mail("dileebandileeban2001@gmail.com","testing","this is a experiment");
+    ?>
+</body>
+</html>
